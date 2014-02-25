df25d93b077389d0c707521a4a286c99003646c2
Hendri Pretorius <pretorh@gmail.com>
2014-02-25 23:05:09 +0200

    fizzbuzz binary

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
 console.log("nothing here yet");
+
+var fb = require("./");
+var fizzbuzz = new fb.FizzBuzz({ write: function(a) { console.log(a); } });
+for (var i = 1; i <= 100; i++) {
+    fizzbuzz.print(i);
+}
